8445c676418c619281d70819581447616fad7d74
Heiko Henning <greenrover33@gmail.com>
2025-12-05 09:41:36 +0100

    Integrate spring auto config

---
+++ src/main/java/community/solace/spring/boot/starter/solaceclientconfig/SolaceJavaProperties.java
@@ -0,0 +1,189 @@
+package community.solace.spring.boot.starter.solaceclientconfig;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ConfigurationProperties("solace.java")
+public class SolaceJavaProperties {
+
+    public static final String SPRING_OAUTH2_CLIENT_REGISTRATION_ID = "SPRING_OAUTH2_CLIENT_REGISTRATION_ID";
+
+    /**
+     * Solace Message Router Host address. Port is optional and intelligently defaulted by the Solace Java API.
+     */
+    private String host = "localhost";
+
+    /**
+     * Solace Message Router Message-VPN.
+     */
+    private String msgVpn = "default";
+
+    /**
+     * Solace Message Router Client Username.
+     * This field is always used in non cloud deployments.
+     * Under a cloud deployment this field is used when the cloud expected value was not provided (null).
+     */
+    private String clientUsername = "spring-default-client-username";
+
+    /**
+     * Solace Message Router Client Password.
+     * This field is always used in non cloud deployments.
+     * Under a cloud deployment this field is used when the cloud expected value was not provided (null).
+     */
+    private String clientPassword;
+
+    /**
+     * The client name to use when connecting to Solace Message Router. This must be unique. If absent, the API will generate a reasonable client name.
+     */
+    private String clientName;
+
+    /**
+     * The number of times to attempt and retry a connection to the host Solace Message Router (or list of routers) during initial connection setup.
+     *
+     * This property is optional and defaults to the suggested value when using HA redundant router pairs as documented in the Solace PubSub+ API Developer Guide.
+     */
+    private int connectRetries = 1;
+
+    /**
+     * The number of times to attempt to reconnect to the host Solace Message Router (or list of routers) after an initial connected session goes down.
+     *
+     * This property is optional and defaults to the suggested value when using HA redundant router pairs as documented in the Solace PubSub+ API Developer Guide.
+     */
+    private int reconnectRetries = -1;
+
+    /**
+     * When using a host list for the HOST property, this property defines how many times to try
+     * to connect or reconnect to a single host before moving to the next host in the list.
+     * NOTE: This property works in conjunction with the connect and reconnect retries settings;
+     * it does not replace them.
+     *
+     * This property is optional and defaults to the suggested value when using HA redundant router pairs as documented in the Solace PubSub+ API Developer Guide.
+     */
+    private int connectRetriesPerHost = 20;
+
+    /**
+     * How much time in (MS) to wait between each attempt to connect or reconnect to a host.
+     * If a connect or reconnect attempt to host is not successful, the API waits for the
+     * amount of time set for reconnectRetryWaitInMillis, and then makes another connect or
+     * reconnect attempt.
+     *
+     * This property is optional and defaults to the suggested value when using HA redundant router pairs as documented in the Solace PubSub+ API Developer Guide.
+     */
+    private int reconnectRetryWaitInMillis = 3000;
+
+
+    /**
+     * API properties can be set by the attribute naming convention used in
+     * fromProperties() and toProperties()
+     *
+     * Example: solace.java.apiProperties.reapply_subscriptions=true
+     */
+    private final Map<String,String> apiProperties = new ConcurrentHashMap<>();
+
+    /**
+     * The Spring Security OAuth2 Client Registration Id
+     * <code>spring.security.oauth2.client.registration.&lt;registration-id&gt;</code> to use for OAuth2
+     * token
+     * retrieval. This field is required when the Solace session is configured to use OAuth2 via
+     * <code>solace.java.apiProperties.authentication_scheme=AUTHENTICATION_SCHEME_OAUTH2</code>
+     */
+    private String oauth2ClientRegistrationId;
+
+    public String getOauth2ClientRegistrationId() {
+        return oauth2ClientRegistrationId;
+    }
+
+    public void setOauth2ClientRegistrationId(String oauth2ClientRegistrationId) {
+        this.oauth2ClientRegistrationId = oauth2ClientRegistrationId;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getClientUsername() {
+        return clientUsername;
+    }
+
+    public void setClientUsername(String clientUsername) {
+        this.clientUsername = clientUsername;
+    }
+
+    public String getClientPassword() {
+        return clientPassword;
+    }
+
+    public void setClientPassword(String clientPassword) {
+        this.clientPassword = clientPassword;
+    }
+
+    public String getMsgVpn() {
+        return msgVpn;
+    }
+
+    public void setMsgVpn(String msgVpn) {
+        this.msgVpn = msgVpn;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public int getConnectRetries() {
+        return connectRetries;
+    }
+
+    public void setConnectRetries(int connectRetries) {
+        this.connectRetries = connectRetries;
+    }
+
+    public int getReconnectRetries() {
+        return reconnectRetries;
+    }
+
+    public void setReconnectRetries(int reconnectRetries) {
+        this.reconnectRetries = reconnectRetries;
+    }
+
+    public int getConnectRetriesPerHost() {
+        return connectRetriesPerHost;
+    }
+
+    public void setConnectRetriesPerHost(int connectRetriesPerHost) {
+        this.connectRetriesPerHost = connectRetriesPerHost;
+    }
+
+    public int getReconnectRetryWaitInMillis() {
+        return reconnectRetryWaitInMillis;
+    }
+
+    public void setReconnectRetryWaitInMillis(int reconnectRetryWaitInMillis) {
+        this.reconnectRetryWaitInMillis = reconnectRetryWaitInMillis;
+    }
+
+    public Map<String,String> getApiProperties() {
+        return apiProperties;
+    }
+
+    public String getStringProperty(String name) {
+        String result = null;
+
+        try {
+            result = (String)this.apiProperties.get(name);
+            return result;
+        } catch (Exception var4) {
+            return null;
+        }
+    }
+
+}
---
+++ src/test/java/community/solace/spring/boot/starter/solaceclientconfig/SolaceOAuthClientConfigurationTest.java
@@ -0,0 +1,105 @@
+package community.solace.spring.boot.starter.solaceclientconfig;
+
+import com.solacesystems.jcsmp.SolaceSessionOAuth2TokenProvider;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolaceOAuthClientConfigurationTest {
+
+    @SpringBootApplication
+    public static class TestApp {
+
+    }
+
+    @Test
+    void verifyApplicationContextContainsRequiredBeans() {
+        try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .profiles("oauthConfigIT").sources(TestApp.class)
+                .properties(
+                        String.format("%s=%s", "solace.java.api-properties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_OAUTH2"),
+                        String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"),
+                        "server.port=0")
+                .run()) {
+            assertThat(context.isRunning()).isTrue();
+            assertThat(context.getBean(SolaceSessionOAuth2TokenProvider.class)).isNotNull();
+            assertThat(
+                    context.getBean(AuthorizedClientServiceOAuth2AuthorizedClientManager.class)).isNotNull();
+        }
+    }
+
+    @Test
+    void verifyApplicationContextContainsRequiredBeans2() {
+        try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .profiles("oauthConfigIT").sources(TestApp.class)
+                .properties(
+                        String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_OAUTH2"),
+                        String.format("%s=%s", "solace.java.oauth2-client-registration-id", "my-oauth2-client"),
+                        "server.port=0")
+                .run()) {
+            assertThat(context.isRunning()).isTrue();
+            assertThat(context.getBean(SolaceSessionOAuth2TokenProvider.class)).isNotNull();
+            assertThat(
+                    context.getBean(AuthorizedClientServiceOAuth2AuthorizedClientManager.class)).isNotNull();
+        }
+    }
+
+    @Test
+    void verifyApplicationContextDoesNotContainOAuth2BeansWhenAuthSchemeIsNotOAuth2() {
+        try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .profiles("oauthConfigIT").sources(TestApp.class)
+                .properties(
+                        String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_BASIC"),
+                        String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"),
+                        "server.port=0")
+                .run()) {
+            assertThat(context.isRunning()).isTrue();
+
+            assertThatThrownBy(() -> context.getBean(SolaceSessionOAuth2TokenProvider.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+            assertThatThrownBy(
+                    () -> context.getBean(AuthorizedClientServiceOAuth2AuthorizedClientManager.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+        }
+    }
+
+    @Test
+    void verifyApplicationContextDoesNotContainOAuth2BeansWhenAuthSchemePropertyNotDefined() {
+        try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .profiles("oauthConfigIT").sources(TestApp.class)
+                .properties(String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"),
+                        "server.port=0")
+                .run()) {
+            assertThat(context.isRunning()).isTrue();
+
+            assertThatThrownBy(() -> context.getBean(SolaceSessionOAuth2TokenProvider.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+            assertThatThrownBy(
+                    () -> context.getBean(AuthorizedClientServiceOAuth2AuthorizedClientManager.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+        }
+    }
+
+    @Test
+    void verifyApplicationContextDoesNotContainOAuth2BeansWhenClientRegistrationIdNotDefined() {
+        try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .profiles("oauthConfigIT").sources(TestApp.class)
+                .properties(String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_OAUTH2"),
+                        "server.port=0")
+                .run()) {
+            assertThat(context.isRunning()).isTrue();
+
+            assertThatThrownBy(() -> context.getBean(SolaceSessionOAuth2TokenProvider.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+            assertThatThrownBy(
+                    () -> context.getBean(AuthorizedClientServiceOAuth2AuthorizedClientManager.class))
+                    .isInstanceOf(NoSuchBeanDefinitionException.class);
+        }
+    }
+}
