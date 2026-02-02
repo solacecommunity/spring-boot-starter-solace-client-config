0da0bdec1a8ae3aeb8ef5c869d3a981a592c41b6
e568981 <samuel.gasser@sbb.ch>
2026-02-02 08:55:31 +0100

    STTRS-2600 Update to Spring Boot 4

--- src/main/java/community/solace/spring/boot/starter/solaceclientconfig/SolaceJavaAutoConfiguration.java
+++ src/main/java/community/solace/spring/boot/starter/solaceclientconfig/SolaceJavaAutoConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,7 +25,6 @@ import java.util.Properties;
 import java.util.Set;
 
 @Configuration
-@AutoConfigureBefore(JmsAutoConfiguration.class)
 @ConditionalOnClass({JCSMPProperties.class})
 @ConditionalOnMissingBean(SpringJCSMPFactory.class)
 @EnableConfigurationProperties({
